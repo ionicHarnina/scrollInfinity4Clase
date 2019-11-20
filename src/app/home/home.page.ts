2a1536c4431ef311e9f33a8ce0390361a0dec777
programacion <>
2019-11-20 10:17:16 +0100

    olvido

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  items: string[] = [];
+  termina = true;
+  constructor() {
+    this.cargarVector();
+  }
+  private cargarVector() {
+    for (let i: number = 0; i < 30; i++) {
+      this.items.push(i.toString());
+    }
+  }
 
-  constructor() {}
+  doInfinite(event: any) {
+    console.log("comenzado operacion asincrona");
+    //Vamos a meter mas elementos en la lista
+    setTimeout(() => {
+      this.cargarVector();
+      console.log("terminando operacion asincrona");
+      //terminar el scroll
+      event.target.complete();
+      if (this.items.length > 100) this.termina = false;
+    }, 500);
+    console.log("fuera del settimeout no espero a nadie");
 
+  }
 }
